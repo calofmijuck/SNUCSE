334b06b2b5a9a97125cc3e0086e1ebd625331ec6
Sungchan Yi <38686321+calofmijuck@users.noreply.github.com>
2018-06-01 23:02:49 +0900

    Add files via upload

---
+++ 2017 Spring/Physics Lab 1/실험1-7. 용수철 흔들이의 운동/plotLiss.m
@@ -0,0 +1,12 @@
+plot(time, x1,'DisplayName','x');
+hold on;
+plot(time, y1,'DisplayName','y');
+hold off;
+legend('x', 'y');
+
+xlabel('Time (s)');
+ylabel('Position (m)');
+
+%plot(x, y);
+%xlabel('x Position (m)');
+%ylabel('y Position (m)');
---
+++ 2017 Spring/Physics Lab 1/실험1-7. 용수철 흔들이의 운동/x_time_init.m
@@ -0,0 +1,4 @@
+n = input('How many pieces of data?: ');
+for i = 1:n
+    time2(i) = 1/30 * (i-1);
+end
