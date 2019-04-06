400cda3dbfda533fa581c1db579ee586469bfa09
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-04-06 21:24:20 +0900

    Create hw1_cpp.h

---
+++ 2019 Spring/Algorithms/hw1_cpp.h
@@ -0,0 +1,14 @@
+//find the "k"th smallest element in array "a" with "n" elements by using Randomized-select in CLRS
+int randomized_select(int a[], int n, int k){
+    return 0;
+}
+
+//find the "k"th smallest element in array "a" with "n" elements by using the worst-case linear-time algorithm in CLRS
+int deterministic_select(int a[], int n, int k){
+    return 0;
+}
+
+//check whether the "k"th smallest element in array "a" with "n" elements is the "ans"
+bool checker(int a[], int n, int k, int ans){
+    return true;
+}
